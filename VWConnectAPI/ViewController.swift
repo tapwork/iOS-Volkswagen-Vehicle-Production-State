0ed7e570c91212f4cc8e626da3d212d362e3bf12
Christian Menschel <christian@tapwork.de>
2021-04-12 00:28:49 +0200

    Use ScrollView

--- VWConnectAPI/ViewController.swift
+++ VWConnectAPI/ViewController.swift
@@ -11,16 +11,66 @@ import Combine
 class ViewController: UIViewController {
 
     var subscriptions = [AnyCancellable]()
+    lazy var scrollView = UIScrollView()
     lazy var stackView = UIStackView()
     lazy var loadingIndicator = UIActivityIndicatorView(style: .large)
     lazy var refreshButton = UIButton()
 
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupScrollView()
+        setupStackView()
+        setupLoadingIndicator()
+        setupRefreshButton()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        load()
+        NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { (notification) in
+            self.load()
+        }
+    }
+
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        scrollView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        stackView.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 20, right: 20)
+        stackView.isLayoutMarginsRelativeArrangement = true
+    }
+
+    private func setupLoadingIndicator() {
         view.addSubview(loadingIndicator)
         loadingIndicator.hidesWhenStopped = true
         loadingIndicator.center = view.center
+    }
 
+    private func setupRefreshButton() {
         view.addSubview(refreshButton)
         refreshButton.translatesAutoresizingMaskIntoConstraints = false
         refreshButton.setTitle("Reload", for: .normal)
@@ -33,66 +83,6 @@ class ViewController: UIViewController {
             refreshButton.widthAnchor.constraint(equalToConstant: 150)
         ])
         refreshButton.addTarget(self, action: #selector(refresh), for: .primaryActionTriggered)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        load()
-        NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { (notification) in
-            self.load()
-        }
-    }
-
-    @objc func refresh() {
-        load()
-    }
-
-    func load() {
-        loadingIndicator.startAnimating()
-        let webViewController = WebViewController(target: .login)
-        present(webViewController, animated: true, completion: nil)
-        webViewController
-            .tokenHandler
-            .flatMap(fetchVehicleState)
-            .sink { (error) in
-                print(error)
-                self.loadingIndicator.stopAnimating()
-            } receiveValue: { states in
-                self.createLabels(for: states)
-            }
-            .store(in: &subscriptions)
-    }
-
-    func createLabels(for states: [VehicleState]) {
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.arrangedSubviews.forEach { (view) in
-            view.removeFromSuperview()
-            stackView.removeArrangedSubview(view)
-        }
-        states.forEach { (state) in
-            stackView.addArrangedSubview(createLabel(title: "brand", text: state.brand))
-            stackView.addArrangedSubview(createLabel(title: "Name", text: state.name))
-            stackView.addArrangedSubview(createLabel(title: "vin", text: state.vin ?? "n.a."))
-            stackView.addArrangedSubview(createLabel(title: "commissionNumber", text: state.commissionNumber))
-            stackView.addArrangedSubview(createLabel(title: "checkpointNumber", text: state.checkpointNumber))
-            stackView.addArrangedSubview(createLabel(title: "detailStatus", text: state.detailStatus))
-            stackView.addArrangedSubview(createLabel(title: "orderStatus", text: state.orderStatus))
-            stackView.addArrangedSubview(createLabel(title: "deliveryDate", text: state.deliveryDateLocalized))
-            stackView.addArrangedSubview(createLabel(title: "orderDate", text: state.orderDateLocalized))
-            stackView.addArrangedSubview(createLabel(title: "modelCode", text: state.modelCode))
-            stackView.addArrangedSubview(createLabel(title: "modelYear", text: state.modelYear))
-        }
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-        ])
-        stackView.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 20, right: 20)
-        stackView.isLayoutMarginsRelativeArrangement = true
     }
 
     func createLabel(title: String, text: String) -> UIStackView {
@@ -115,11 +105,59 @@ class ViewController: UIViewController {
         return stackView
     }
 
+    func createSpacer() -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        return view
+    }
+
     func fetchVehicleState(_ token: String) -> AnyPublisher<[VehicleState], Error> {
         URLSession.shared.dataTaskPublisher(for: URLTarget.state.request(accessToken: token))
             .map {$0.data}
             .decode(type: [VehicleState].self, decoder: JSONDecoder.shared)
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
+    }
+
+    // MARK: Actions
+    @objc func refresh() {
+        load()
+    }
+
+    func load() {
+        loadingIndicator.startAnimating()
+        let webViewController = WebViewController(target: .login)
+        present(webViewController, animated: true, completion: nil)
+        webViewController
+            .tokenHandler
+            .flatMap(fetchVehicleState)
+            .sink { (error) in
+                print(error)
+                self.loadingIndicator.stopAnimating()
+            } receiveValue: { states in
+                self.createContentViews(for: states)
+            }
+            .store(in: &subscriptions)
+    }
+
+    func createContentViews(for states: [VehicleState]) {
+        stackView.arrangedSubviews.forEach { (view) in
+            view.removeFromSuperview()
+            stackView.removeArrangedSubview(view)
+        }
+        states.forEach { (state) in
+            stackView.addArrangedSubview(createLabel(title: "Brand", text: state.brand))
+            stackView.addArrangedSubview(createLabel(title: "Name", text: state.name))
+            stackView.addArrangedSubview(createLabel(title: "VIN", text: state.vin ?? "n.a."))
+            stackView.addArrangedSubview(createLabel(title: "Commission Number", text: state.commissionNumber))
+            stackView.addArrangedSubview(createLabel(title: "Checkpoint Number", text: state.checkpointNumber))
+            stackView.addArrangedSubview(createLabel(title: "Detail Status", text: state.detailStatus))
+            stackView.addArrangedSubview(createLabel(title: "Order Status", text: state.orderStatus))
+            stackView.addArrangedSubview(createLabel(title: "Delivery Date", text: state.deliveryDateLocalized))
+            stackView.addArrangedSubview(createLabel(title: "Order Date", text: state.orderDateLocalized))
+            stackView.addArrangedSubview(createLabel(title: "Model Code", text: state.modelCode))
+            stackView.addArrangedSubview(createLabel(title: "Model Year", text: state.modelYear))
+            stackView.addArrangedSubview(createSpacer())
+        }
     }
 }
